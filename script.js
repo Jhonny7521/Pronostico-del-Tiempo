0cfbfbaee1010b5f33119bbb2ae6000b91ea4ec6
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-02-22 15:31:47 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,57 @@
+function alertMessage(obj){
+    let objeto = obj.innerHTML;
+    alert(`Cargando el clima de ${objeto} ...`);
+
+    
+}
+
+function hide(obj){
+    let objeto = obj.parentNode.parentNode;
+    objeto.remove();
+
+}
+
+function Convert(obj){
+    let tipoTemp = obj.value;
+    let tempMax;
+
+    if (tipoTemp == "C"){
+        tempMin = document.getElementsByClassName('tempMin');
+        tempMax = document.getElementsByClassName('tempMax');
+        for(let i=0; i<tempMax.length ; i++){
+            let valorTemp = parseInt(tempMax[i].innerHTML);
+            let resultado;
+            resultado = (valorTemp -32 ) * (5/9);
+            tempMax[i].innerHTML = resultado.toFixed(0);
+        }   
+
+        for(let i=0; i<tempMin.length ; i++){
+            let valorTemp = parseInt(tempMin[i].innerHTML);
+            let resultado;
+            resultado = (valorTemp -32 ) * (5/9);
+            tempMin[i].innerHTML = resultado.toFixed(0);
+        }
+    }
+
+    else if(tipoTemp =="F"){
+        tempMin = document.getElementsByClassName('tempMin');
+        tempMax = document.getElementsByClassName('tempMax');
+        for(let i=0; i<tempMax.length ; i++){
+            let valorTemp = parseInt(tempMax[i].innerHTML);
+            let resultado = (valorTemp * (9/5)) + 32;            
+            tempMax[i].innerHTML = resultado.toFixed(0);
+        }
+
+        for(let i=0; i<tempMin.length ; i++){
+            let valorTemp = parseInt(tempMin[i].innerHTML);
+            let resultado = (valorTemp * (9/5)) + 32;
+            tempMin[i].innerHTML = resultado.toFixed(0);
+        }
+    }
+
+    console.log(tempMax[0].innerHTML);
+    console.log(tempMin[0].innerHTML);
+    console.log(tempMax.length);
+    console.log(tempMin.length);
+    console.log(tipoTemp);
+}
